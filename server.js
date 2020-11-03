a1e1767c6d3627c2b3de6a452ff783ce794012f2
cankush625 <cankush625@gmail.com>
2020-11-04 01:02:33 +0530

    Express API Endpoint and Listener created
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ import express from 'express'
 import mongoose from 'mongoose'
 
 // App Config
+const app = express();
+const port = process.env.PORT || 8001;
 
 // Middlewares
 
 // DB config
 
 // API Endpoints
+app.get('/', (req, res) => {
+    res.status(200).send("Hello, Ankush!")
+});
 
 // Listener
+app.listen(port, () => {
+    console.log(`listening in localhost: ${port}`)
+});
